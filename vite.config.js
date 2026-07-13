4c6d40955068b9f5f6adbb062e9a774d75071b3a
dexteon <140340948+dexteon@users.noreply.github.com>
2026-07-12 21:54:38 -0400

    Added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',
+  server: {
+    port: 5173,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        medical: './medical/index.html',
+        steam: './steam/index.html',
+        'email-agency': './email-agency/index.html',
+        'email-digest': './email-digest/index.html',
+        'finance': './finance/index.html',
+        'comp-playground': './finance/comp-playground.html',
+        'instacart': './instacart/dashboard.html',
+        'playgrounds': './playgrounds/index.html',
+        'va-claim': './playgrounds/va-claim.html',
+        'headache-log': './playgrounds/headache-log.html',
+      }
+    }
+  }
+})
